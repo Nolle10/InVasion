90199e2fc65094aa25965edb22bde53ba81e05fb
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2023-03-03 10:49:21 +0100

    Fixed underlying issues

---
+++ Core/src/main/java/gamestates/GameState.java
@@ -0,0 +1,20 @@
+package gamestates;
+
+import managers.GameStateManager;
+
+public abstract class GameState {
+
+    protected GameStateManager gsm;
+
+    protected GameState(GameStateManager gsm) {
+        this.gsm = gsm;
+        init();
+    }
+
+    public abstract void init();
+    public abstract void update(float dt);
+    public abstract void draw();
+    public abstract void handleInput();
+    public abstract void dispose();
+
+}
---
+++ Core/src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LwjglApplicationConfiguration cfg =
+                new LwjglApplicationConfiguration();
+        cfg.title = "InVasion";
+        cfg.width = 500;
+        cfg.height = 400;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(new Game(), cfg);
+
+    }
+}
