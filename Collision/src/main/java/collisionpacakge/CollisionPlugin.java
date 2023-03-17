ee32750196705e2aba603c7955d0ac1047fc3bc8
Mads Pedersen <mads456dk@gmali.com>
2023-03-17 13:51:32 +0100

    It should build now

--- Collision/src/main/java/collisionpacakge/CollisionPlugin.java
+++ Collision/src/main/java/collisionpacakge/CollisionPlugin.java
@@ -1,6 +1,17 @@
 package collisionpacakge;
 
+import data.GameData;
+import data.World;
 import services.PluginService;
 
 public class CollisionPlugin implements PluginService {
+    @Override
+    public void onEnable(GameData data, World world) {
+
+    }
+
+    @Override
+    public void onDisable(GameData data, World world) {
+
+    }
 }
