ac74c71733b5b592912644dbee445405fd291fc7
Katrine Birk <katrinebirk2@live.dk>
2023-03-24 16:16:07 +0100

    Cleanup

--- AI/src/main/java/dk/sdu/se/f23/InVasion/AI/AIAction.java
+++ AI/src/main/java/dk/sdu/se/f23/InVasion/AI/AIAction.java
@@ -2,11 +2,9 @@ package dk.sdu.se.f23.InVasion.AI;
 
 import data.GameData;
 import data.World;
-import data.entityparts.MovingPart;
 import dk.sdu.se.f23.InVasion.enemy.Enemy;
 import dk.sdu.se.f23.InVasion.enemy.services.ActionService;
 
-import java.util.Random;
 
 public class AIAction implements ActionService {
     @Override
