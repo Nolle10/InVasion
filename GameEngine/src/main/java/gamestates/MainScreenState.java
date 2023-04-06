83638f826114913e30765658f09c51275780d358
CamillaFrank <75301585+CamillaFrank@users.noreply.github.com>
2023-04-06 22:04:24 +0200

    added basic buttons/paths between states

--- GameEngine/src/main/java/gamestates/MainScreenState.java
+++ GameEngine/src/main/java/gamestates/MainScreenState.java
@@ -1,17 +1,44 @@
 package gamestates;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import managers.GameStateManager;
 
 public class MainScreenState extends GameState{
+
     private Stage stage;
-    protected MainScreenState(GameStateManager gsm) {
+    private TextButton button;
+    private TextButton.TextButtonStyle textButtonStyle;
+
+    public MainScreenState(GameStateManager gsm) {
         super(gsm);
     }
 
     @Override
     public void init() {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = new BitmapFont();
+        textButtonStyle.fontColor = Color.WHITE;
+        button = new TextButton("Play game button", textButtonStyle);
+        button.setPosition(900,500);
+        button.addListener( new InputListener(){
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button){
+                System.out.println("Start button clicked!");
+                gsm.setState(1);
+                return true;
+            }
+        });
+
+        stage.addActor(button);
     }
 
     @Override
@@ -21,7 +48,7 @@ public class MainScreenState extends GameState{
 
     @Override
     public void draw() {
-
+        stage.draw();
     }
 
     @Override
