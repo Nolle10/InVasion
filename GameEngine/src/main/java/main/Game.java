8340d9663a95c6f4cfd870b0afd7b2098343be48
Katrine Birk <katrinebirk2@live.dk>
2023-04-07 09:43:46 +0200

    Game.java with temp module loading

--- GameEngine/src/main/java/main/Game.java
+++ GameEngine/src/main/java/main/Game.java
@@ -3,7 +3,17 @@ package main;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
+import data.GameData;
+import data.ProcessAt;
+import data.World;
+import dk.sdu.se.f23.InVasion.enemy.EnemyControlSystem;
+import dk.sdu.se.f23.InVasion.enemy.EnemyPlugin;
 import managers.GameStateManager;
+import services.EntityProcessingService;
+import services.PluginService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game implements ApplicationListener {
     public static int WIDTH;
@@ -13,16 +23,37 @@ public class Game implements ApplicationListener {
 
     private GameStateManager gsm;
 
+    //ENEMY TEST
+    private final GameData gameData = new GameData();
+    private List<EntityProcessingService> entityProcessors = new ArrayList<>();
+    private List<PluginService> entityPlugins = new ArrayList<>();
+    private World world = new World();
+
     public void create() {
 
         WIDTH = Gdx.graphics.getWidth();
         HEIGHT = Gdx.graphics.getHeight();
+        gameData.setDisplayWidth(WIDTH);
+        gameData.setDisplayHeight(HEIGHT);
 
         cam = new OrthographicCamera(WIDTH, HEIGHT);
         cam.translate(WIDTH / 2, HEIGHT / 2);
         cam.update();
 
         gsm = new GameStateManager();
+
+        //ENEMY TEST
+        PluginService enemyPlugin = new EnemyPlugin();
+
+
+        EntityProcessingService enemyProcess = new EnemyControlSystem();
+
+        entityPlugins.add(enemyPlugin);
+        entityProcessors.add(enemyProcess);
+        // Lookup all Game Plugins using ServiceLoader
+        for (PluginService iGamePlugin : entityPlugins) {
+            iGamePlugin.onEnable(gameData, world);
+        }
 
     }
 
@@ -35,6 +66,18 @@ public class Game implements ApplicationListener {
         draw();
         gsm.draw();
 
+        //ENEMY TEST
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+        update();
+
+    }
+
+    //ENEMY TEST
+    private void update() {
+        // Update
+        for (EntityProcessingService entityProcessorService : entityProcessors) {
+            entityProcessorService.process(gameData, world, ProcessAt.Tick);
+        }
     }
     public void draw(){
 
