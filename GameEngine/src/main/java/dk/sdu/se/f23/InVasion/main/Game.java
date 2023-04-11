fe0f58c84b931ea018f2e47a28b930bc36b0733e
Katrine Birk <katrinebirk2@live.dk>
2023-04-11 08:45:21 +0200

    Removed module-info for demo purposes (and other related changes)

--- GameEngine/src/main/java/dk/sdu/se/f23/InVasion/main/Game.java
+++ GameEngine/src/main/java/dk/sdu/se/f23/InVasion/main/Game.java
@@ -8,9 +8,15 @@ import dk.sdu.se.f23.InVasion.common.data.ProcessAt;
 import dk.sdu.se.f23.InVasion.common.data.World;
 import dk.sdu.se.f23.InVasion.common.services.EntityProcessingService;
 import dk.sdu.se.f23.InVasion.common.services.PluginService;
+import dk.sdu.se.f23.InVasion.enemy.EnemyControlSystem;
+import dk.sdu.se.f23.InVasion.enemy.EnemyPlugin;
 import dk.sdu.se.f23.InVasion.managers.GameStateManager;
+import dk.sdu.se.f23.InVasion.player.PlayerControlSystem;
+import dk.sdu.se.f23.InVasion.player.PlayerPlugin;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.ServiceLoader;
 
 import static java.util.stream.Collectors.toList;
@@ -21,6 +27,10 @@ public class Game implements ApplicationListener {
     public static int HEIGHT;
 
     public static OrthographicCamera cam;
+
+    //Only for demo - remove later
+    private List<PluginService> pluginServices = new ArrayList<>();
+    private List<EntityProcessingService> entityProcessingServices = new ArrayList<>();
 
     private GameStateManager gsm;
     private final GameData gameData = new GameData();
@@ -40,14 +50,26 @@ public class Game implements ApplicationListener {
 
         gsm = new GameStateManager();
 
-        for (PluginService plugin : getPluginServices()) {
+        //Only for demo - remove later
+        PluginService enemyPlugin = new EnemyPlugin();
+        EntityProcessingService enemyProcess = new EnemyControlSystem();
+        pluginServices.add(enemyPlugin);
+        entityProcessingServices.add(enemyProcess);
+        PluginService playerPlugin = new PlayerPlugin();
+        EntityProcessingService playerProcess = new PlayerControlSystem();
+        pluginServices.add(playerPlugin);
+        entityProcessingServices.add(playerProcess);
+
+        for (PluginService plugin : pluginServices) {
+            plugin.onEnable(gameData, world);
+        }
+        /*for (PluginService plugin : getPluginServices()) {
             plugin.onEnable(gameData, world);
             System.out.println(plugin.getClass().getName() + " loaded");
-        }
+        }*/
     }
 
     public void render() {
-        System.out.println("Render");
 
         // clear screen to black
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -62,7 +84,11 @@ public class Game implements ApplicationListener {
 
     //Update method for EntityProcessingServices: How to do it with ProcessAt.Tick and ProcessAt values?
     private void update(ProcessAt processAt){
-        for (EntityProcessingService entityProcessor : getEntityProcessingServices()) {
+        /*for (EntityProcessingService entityProcessor : getEntityProcessingServices()) {
+            entityProcessor.process(gameData, world, processAt);
+        }*/
+        //For demo - remove later
+        for (EntityProcessingService entityProcessor : entityProcessingServices) {
             entityProcessor.process(gameData, world, processAt);
         }
     }
@@ -74,10 +100,11 @@ public class Game implements ApplicationListener {
     public void dispose() {}
 
     //ServiceLoader - Loads in all Plugin services and EntityProcessing services
+    /*
     private Collection<? extends PluginService> getPluginServices() {
         return ServiceLoader.load(PluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
     }
     private Collection<? extends EntityProcessingService> getEntityProcessingServices() {
         return ServiceLoader.load(EntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
+    }*/
 }
