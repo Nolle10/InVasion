eb5d400b125d86b26d45e4f3145484df3a2aede4
Katrine Birk <katrinebirk2@live.dk>
2023-04-28 17:03:35 +0200

    added shooting event to weapon

--- Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponControlSystem.java
+++ Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponControlSystem.java
@@ -4,17 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import dk.sdu.se.f23.InVasion.common.data.*;
 import dk.sdu.se.f23.InVasion.common.data.entityparts.PositionPart;
-import dk.sdu.se.f23.InVasion.common.events.BuyTowerEvent;
-import dk.sdu.se.f23.InVasion.common.events.Event;
-import dk.sdu.se.f23.InVasion.common.events.EventListener;
+import dk.sdu.se.f23.InVasion.common.events.*;
 import dk.sdu.se.f23.InVasion.common.services.EntityProcessingService;
 import dk.sdu.se.f23.InVasion.commonweapon.Weapon;
 
 public class WeaponControlSystem implements EntityProcessingService, EventListener {
+    private long lastShot = 0;
 
     @Override
     public void process(GameData data, World world, ProcessAt processTime) {
         for (Entity weapon : world.getEntities(Weapon.class)) {
+            lastShot += data.getDelta()*50;
+            if (lastShot >= 2) {
+                EventDistributor.sendEvent(new FireShotEvent(weapon/*,getDirection()*/),world);
+                lastShot = 0;
+            }
         }
     }
 
@@ -23,6 +27,11 @@ public class WeaponControlSystem implements EntityProcessingService, EventListen
         weapon.add(new PositionPart(new Point(position.getX(),position.getY()),0));
         weapon.setTexture(new Texture(Gdx.files.internal("Weapon/src/main/resources/TOWER.png")));
         return weapon;
+    }
+
+    private Point getDirection(){
+        //TODO: Get position for enemy and shoot
+        return new Point(0,0);
     }
 
     @Override
