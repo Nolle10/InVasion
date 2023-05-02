23149cc250f2c85eb7d0094fb81ebe5685a3a940
Katrine Birk <katrinebirk2@live.dk>
2023-05-02 10:41:42 +0200

    Adjusted nearest neighbor. Does not find TargetEvent sources correctly

--- Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponControlSystem.java
+++ Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponControlSystem.java
@@ -14,20 +14,20 @@ import java.util.List;
 
 public class WeaponControlSystem implements EntityProcessingService, EventListener {
     private long lastShot = 0;
-    private Point enemyPosition;
     private List<Entity> targets = new ArrayList<>();
 
     @Override
     public void process(GameData data, World world, ProcessAt processTime) {
         for (Entity weapon : world.getEntities(Weapon.class)) {
+            Point direction = findNearestNeighbor(world);
             lastShot += data.getDelta() * 50;
-//TODO: Figure out why enemyPosition is null
-            if (enemyPosition != null) {
-                System.out.println("Fire weapon from "+ enemyPosition.getX() + " " + enemyPosition.getY());
-                EventDistributor.sendEvent(new FireShotEvent(weapon, getDirection()), world);
-                lastShot = 0;
-            } else{
-                System.out.println("No target");
+            if (lastShot >= 0.5) {
+                if (direction != null) {
+                    EventDistributor.sendEvent(new FireShotEvent(weapon, direction), world);
+                    lastShot = 0;
+                } else {
+                    System.out.println("No target");
+                }
             }
             PositionPart part = weapon.getPart(PositionPart.class);
             data.getSpriteBatch().draw(weapon.getTexture(), part.getX(), part.getY());
@@ -41,9 +41,6 @@ public class WeaponControlSystem implements EntityProcessingService, EventListen
         return weapon;
     }
 
-    private Point getDirection() {
-        return enemyPosition;
-    }
 
     @Override
     public void processEvent(Event event, World world) {
@@ -52,40 +49,47 @@ public class WeaponControlSystem implements EntityProcessingService, EventListen
         }
         if (event instanceof TargetEvent) {
             targets.add(event.getSource());
-            enemyPosition = ((TargetEvent) event).getTarget();
-            findNearestNeighbor(world);
+            cleanEnemies();
         }
     }
 
-    private void findNearestNeighbor(World world){
+    private Point findNearestNeighbor(World world) {
         //Implementation of Nearest Neighbor algorithm
-        for (Entity weapon : world.getEntities(Weapon.class)) {
-            float weaponX = ((PositionPart) weapon.getPart(PositionPart.class)).getPos().getX();
-            float weaponY = ((PositionPart) weapon.getPart(PositionPart.class)).getPos().getY();
-            double closestDistance = Double.MAX_VALUE;
-            Entity closestTarget = null;
-            for (Entity target : targets) {
-                if (!((LifePart) target.getPart(LifePart.class)).isDead()) {
-                    float targetX = ((PositionPart) target.getPart(PositionPart.class)).getPos().getX();
-                    float targetY = ((PositionPart) target.getPart(PositionPart.class)).getPos().getY();
-                    double distance = Math.sqrt(Math.pow(weaponX - targetX, 2) + Math.pow(weaponY - targetY, 2));
-                    if (distance < closestDistance) {
-                        closestDistance = distance;
-                        closestTarget = target;
+        if (!targets.isEmpty()) {
+            Point enemyPosition = null;
+            for (Entity weapon : world.getEntities(Weapon.class)) {
+                float weaponX = ((PositionPart) weapon.getPart(PositionPart.class)).getPos().getX();
+                float weaponY = ((PositionPart) weapon.getPart(PositionPart.class)).getPos().getY();
+                double closestDistance = Double.MAX_VALUE;
+                Entity closestTarget = null;
+                for (Entity target : targets) {
+                    System.out.println("Target: " + ((PositionPart) target.getPart(PositionPart.class)).getX() + " " + ((PositionPart) target.getPart(PositionPart.class)).getY());
+                    if (!((LifePart) target.getPart(LifePart.class)).isDead()) {
+                        float targetX = ((PositionPart) target.getPart(PositionPart.class)).getPos().getX();
+                        float targetY = ((PositionPart) target.getPart(PositionPart.class)).getPos().getY();
+                        double distance = Math.sqrt(Math.pow(weaponX - targetX, 2) + Math.pow(weaponY - targetY, 2));
+                        if (distance < closestDistance) {
+                            closestDistance = distance;
+                            closestTarget = target;
+                        }
                     }
                 }
-            }
-            if (closestTarget != null) {
-                float closestX = ((PositionPart) closestTarget.getPart(PositionPart.class)).getPos().getX();
-                float closestY = ((PositionPart) closestTarget.getPart(PositionPart.class)).getPos().getY();
-                System.out.println("Closest target: " + closestX + " " + closestY);
-                if (enemyPosition == null) {
+                if (closestTarget != null) {
+                    float closestX = ((PositionPart) closestTarget.getPart(PositionPart.class)).getPos().getX();
+                    float closestY = ((PositionPart) closestTarget.getPart(PositionPart.class)).getPos().getY();
+                    System.out.println("Closest target: " + closestX + " " + closestY);
                     enemyPosition = new Point((int) closestX, (int) closestY);
+                } else {
+                    enemyPosition = new Point(0, 0);
                 }
-                enemyPosition.setX((int) closestX);
-                enemyPosition.setY((int) closestY);
             }
-        } cleanEnemies();
+            cleanEnemies();
+            assert enemyPosition != null;
+            System.out.println("Enemy position: " + enemyPosition.getX() + " " + enemyPosition.getY());
+            return enemyPosition;
+        }
+        System.out.println("No target");
+        return null;
     }
 
     private void cleanEnemies() {
