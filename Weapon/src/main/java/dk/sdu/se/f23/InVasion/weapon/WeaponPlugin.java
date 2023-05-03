743ba5a6ec51b6158bb340d4d5012419376028a1
Patrick Larsen <patrickbjerre@outlook.dk>
2023-05-03 10:20:59 +0200

    small changes

--- Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponPlugin.java
+++ Weapon/src/main/java/dk/sdu/se/f23/InVasion/weapon/WeaponPlugin.java
@@ -2,19 +2,15 @@ package dk.sdu.se.f23.InVasion.weapon;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import dk.sdu.se.f23.InVasion.common.data.Entity;
 import dk.sdu.se.f23.InVasion.common.data.GameData;
-import dk.sdu.se.f23.InVasion.common.data.ProcessAt;
 import dk.sdu.se.f23.InVasion.common.data.World;
-import dk.sdu.se.f23.InVasion.common.services.EntityProcessingService;
 import dk.sdu.se.f23.InVasion.common.services.PluginService;
 
 import java.util.ArrayList;
-import java.util.Objects;
+
 
 public class WeaponPlugin implements PluginService {
-    private String weaponName = "Steven";
+    private final String weaponName = "Steven";
     private Texture texture = new Texture(Gdx.files.internal("Weapon/src/main/resources/TOWER.png"));
     private int cost = 200;
 
