ac7ce37e0258204c02e43dd57fec85a196859138
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2023-05-12 14:35:31 +0200

    Added some test classes

---
+++ Collision/src/test/java/dk/sdu/se/f23/InVasion/collision/CollisionDetectorTest.java
@@ -0,0 +1,13 @@
+package dk.sdu.se.f23.InVasion.collision;
+
+import dk.sdu.se.f23.InVasion.common.data.Entity;
+import dk.sdu.se.f23.InVasion.commonenemy.Enemy;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollisionDetectorTest {
+
+
+}
---
+++ GameEngine/src/test/java/dk/sdu/se/f23/InVasion/managers/GameStateManagerTest.java
@@ -0,0 +1,19 @@
+package dk.sdu.se.f23.InVasion.managers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GameStateManagerTest {
+
+    @Test
+    void setState() {
+        GameStateManager actualGameState = mock(GameStateManager.class);
+
+        actualGameState.setState(3);
+
+        assertEquals(3, actualGameState.getCurrentState());
+
+    }
+}
