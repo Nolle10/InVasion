5c064a11d22e95324866a13d26aa8e92dc5535ca
Patrick Larsen <patrickbjerre@outlook.dk>
2023-05-19 14:49:19 +0200

    something

--- Bullet/src/main/java/dk/sdu/se/f23/InVasion/bullet/BulletController.java
+++ Bullet/src/main/java/dk/sdu/se/f23/InVasion/bullet/BulletController.java
@@ -18,19 +18,27 @@ import dk.sdu.se.f23.InVasion.common.services.EntityProcessingService;
 import dk.sdu.se.f23.InVasion.commonbullet.Bullet;
 
 public class BulletController implements EntityProcessingService, EventListener {
+    private GameData gameData = new GameData();
     private GameStateEnum lastKnownState;
 
     public BulletController() {
         EventDistributor.addListener(StateChangeEvent.class, this);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public void process(GameData data, World world, ProcessAt processTime) {
         for (Entity bullet : world.getEntities(Bullet.class)) {
-            processParts(data, bullet);
+            PositionPart positionPart = bullet.getPart(PositionPart.class);
+            TimerPart timerPart = bullet.getPart(TimerPart.class);
+            MovingPart movingPart = bullet.getPart(MovingPart.class);
+            if (timerPart.getDuration() < 0) {
+                world.removeEntity(bullet);
+            }
 
-            if (((TimerPart) bullet.getPart(TimerPart.class)).getDuration() < 0) {
+            timerPart.process(data, bullet);
+            positionPart.process(data, bullet);
+            movingPart.process(data, bullet);
+            if (timerPart.getDuration() < 0) {
                 world.removeEntity(bullet);
             }
 
@@ -38,36 +46,27 @@ public class BulletController implements EntityProcessingService, EventListener 
         }
     }
 
-    private void processParts(GameData data, Entity bullet) {
-        bullet.getPart(PositionPart.class).process(data, bullet);
-        bullet.getPart(MovingPart.class).process(data, bullet);
-        bullet.getPart(TimerPart.class).process(data, bullet);
-        bullet.getPart(MovingPart.class).process(data, bullet);
-    }
-
     private void updateShape(Entity bullet, GameData data) {
         PositionPart bulletPos = bullet.getPart(PositionPart.class);
+        MovingPart bulletMoving = bullet.getPart(MovingPart.class);
 
+        bulletMoving.process(data,bullet);
         float x = bulletPos.getX();
         float y = bulletPos.getY();
 
-        data.getSpriteBatch().draw(bullet.getTexture(), x, y, Gdx.graphics.getWidth() / 80f, Gdx.graphics.getHeight() / 45f);
+
+        data.getSpriteBatch().draw(bullet.getTexture(), x, y,Gdx.graphics.getWidth()/80, Gdx.graphics.getHeight()/45);
     }
 
     public Entity createBullet(Entity shooter, Point direction) {
         PositionPart shooterPos = shooter.getPart(PositionPart.class);
 
+        float shooterPosX = shooterPos.getPos().getX();
+        float shooterPosY = shooterPos.getPos().getY();
+        float radians = shooterPos.getRadians();
         Entity bullet = new Bullet();
 
-        addParts(direction, shooterPos, bullet);
-
-        bullet.setTexture(new Texture(Gdx.files.internal("Bullet/src/main/resources/antibodyCut.png")));
-
-        return bullet;
-    }
-
-    private void addParts(Point direction, PositionPart shooterPos, Entity bullet) {
-        bullet.add(new PositionPart(shooterPos.getPos(), shooterPos.getRadians()));
+        bullet.add(new PositionPart(new Point((int) shooterPosX, (int) shooterPosY), radians));
         bullet.add(new MovingPart(direction, 100, 100));
         bullet.add(new LifePart(1));
         bullet.add(new TimerPart(3));
@@ -76,15 +75,15 @@ public class BulletController implements EntityProcessingService, EventListener 
         Sound sound = Gdx.audio.newSound(Gdx.files.internal("Bullet/src/main/resources/pew.mp3"));
         sound.play();
         return bullet;
-
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public void processEvent(Event event, World world) {
-        if (event instanceof StateChangeEvent stateChangeEvent) {
+        if (event instanceof StateChangeEvent stateChangeEvent){
             this.lastKnownState = stateChangeEvent.getNewState();
         }
+
         if (lastKnownState != GameStateEnum.PlayState) {
             for (Entity bullet : world.getEntities(Bullet.class)) {
                 world.removeEntity(bullet);
@@ -92,7 +91,7 @@ public class BulletController implements EntityProcessingService, EventListener 
             return;
         }
         if (event instanceof FireShotEvent fireShotEvent) {
-            world.addEntity(createBullet(fireShotEvent.getSource(), fireShotEvent.getDirection()));
+            world.addEntity(createBullet(fireShotEvent.getSource(),fireShotEvent.getDirection()));
         }
     }
 }
